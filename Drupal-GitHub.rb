a77c7b39d21e2a953c2ba05fc9a4d03cce4a2d36
Stephen Russett <stephen.russett@ottawa.ca>
2013-12-06 23:20:13 -0500

    Refactor for full 1 way sync support

--- Drupal-GitHub.rb
+++ Drupal-GitHub.rb
@@ -1,18 +1,36 @@
-
 require 'octokit'
 require 'feedzirra'
 require 'nokogiri'
 require 'open-uri'
-require 'uri'
 
 class DrupalGitHubIssues
+
+	def initialize(ghUsername, ghPassword, ghRepo, drupalProject)
+		self.gh_Authenticate(ghUsername, ghPassword)
+		drupalData = self.get_Drupal_Issues(drupalProject)
+		gitHubData = self.get_GH_Issues(ghRepo)
+		self.determine_Drupal_GH_Changes(ghRepo, drupalData, gitHubData )
+	end
 
 	def gh_Authenticate(username, password)
 		@ghClient = Octokit::Client.new(:login => username.to_s, :password => password.to_s, :auto_paginate => true)
 	end
 
-	def create_GH_Issue(repo, title, body, options)
-		@ghClient.create_issue(repo.to_s, title.to_s, body.to_s, options)
+	def create_GH_Issue(drupalIssueStatus, ghRepo, title, body, options)
+
+		case drupalIssueStatus.to_s
+			when "Fixed"
+				createdIssue = @ghClient.create_issue(ghRepo.to_s, title.to_s, body.to_s, options)
+				self.close_GH_Issue(ghRepo.to_s, createdIssue.attrs[:number], drupalIssueStatus)
+			when "Closed (fixed)"
+				createdIssue = @ghClient.create_issue(ghRepo.to_s, title.to_s, body.to_s, options)
+				self.close_GH_Issue(ghRepo.to_s, createdIssue.attrs[:number], drupalIssueStatus)
+			when "Closed (works as designed)"
+				createdIssue = @ghClient.create_issue(ghRepo.to_s, title.to_s, body.to_s, options)
+				self.close_GH_Issue(ghRepo.to_s, createdIssue.attrs[:number], drupalIssueStatus)
+			else
+				createdIssue = @ghClient.create_issue(ghRepo.to_s, title.to_s, body.to_s, options)
+		end
 	end
 
 	def close_GH_Issue(repo, issueNumber, closeCode)
@@ -31,8 +49,8 @@ class DrupalGitHubIssues
 		@ghClient.add_comment(repo, issueNumber, commentText.to_s)
 	end
 
-	def get_Drupal_Issues(drupalProject, ghRepo, storeInGH = true, sync = true)
-		rssURL = "https://drupal.org/project/issues/rss/#{drupalProject}"
+	def get_Drupal_Issues(drupalProject)
+		rssURL = "https://drupal.org/project/issues/rss/#{drupalProject}?text=&status=All&priorities=All&categories=All&version=All&component=All"
 		feed = Feedzirra::Feed.fetch_and_parse(rssURL)
 
 		entriesAll = feed.entries
@@ -42,6 +60,7 @@ class DrupalGitHubIssues
 		entriesAll.each do |entry|
 			page = Nokogiri::HTML(open(entry.url.to_s))
 			drupalIssueID = URI(entry.url).path.split('/').last
+			
 			drupalIssueComponent = page.xpath(".//*[@id='block-project-issue-issue-metadata']/div/div/div[4]/div[2]/div/text()")
 			drupalIssueVersion = page.xpath(".//*[@id='block-project-issue-issue-metadata']/div/div/div[3]/div[2]/div/text()")
 			drupalIssueStatus = page.xpath(".//*[@id='block-project-issue-issue-metadata']/div/div/div[1]/div/div/text()") 
@@ -53,25 +72,8 @@ class DrupalGitHubIssues
 			entry.summary.concat("<br> <strong>To See Full Issue Go to: #{entry.url.to_s}</strong> <br><br> Issue Originally Created By: <strong>#{drupalIssueCreatedUserName.to_s}</strong>  at  <strong>#{drupalIssueCreateDateTime.to_s}</strong> <br> Component: #{drupalIssueComponent.to_s} <br> Version: #{drupalIssueVersion.to_s} <br> Priority: #{drupalIssuePriority.to_s} <br> Category: #{drupalIssueCategory.to_s} <br> Status: #{drupalIssueStatus.to_s} <br> Drupal Issue Node ID: #{drupalIssueID.to_s}")
 			entry.title.concat("  (#{drupalIssueID})")
 
-			if storeInGH == true
-				case drupalIssueStatus.to_s
-					when "Fixed"
-						createdIssue = self.create_GH_Issue(ghRepo, entry.title, entry.summary, :labels => [drupalIssueComponent.to_s, drupalIssueCategory.to_s, drupalIssuePriority.to_s, drupalIssueVersion.to_s])
-						close_GH_Issue(ghRepo.to_s, createdIssue.attrs[:number].to_s, drupalIssueStatus)
-					when "Closed (fixed)"
-						createdIssue = self.create_GH_Issue(ghRepo, entry.title, entry.summary, :labels => [drupalIssueComponent.to_s, drupalIssueCategory.to_s, drupalIssuePriority.to_s, drupalIssueVersion.to_s])
-						close_GH_Issue(ghRepo.to_s, createdIssue.attrs[:number].to_s, drupalIssueStatus)
-					when "Closed (works as designed)"
-						createdIssue = self.create_GH_Issue(ghRepo, entry.title, entry.summary, :labels => [drupalIssueComponent.to_s, drupalIssueCategory.to_s, drupalIssuePriority.to_s, drupalIssueVersion.to_s])
-						close_GH_Issue(ghRepo.to_s, createdIssue.attrs[:number].to_s, drupalIssueStatus)
-					else
-						createdIssue = self.create_GH_Issue(ghRepo, entry.title, entry.summary, :labels => [drupalIssueComponent.to_s, drupalIssueCategory.to_s, drupalIssuePriority.to_s, drupalIssueVersion.to_s])
-				end
-			else
-				drupalIssues << {:issueNumber => drupalIssueID.to_i, :component => drupalIssueComponent.to_s, :version => drupalIssueVersion.to_s, :status => drupalIssueStatus.to_s, :priority => drupalIssuePriority.to_s, :category => drupalIssueCategory.to_s, :title => entry.title.to_s, :summary => entry.summary.to_s}
-			end
+			drupalIssues << {:issueNumber => drupalIssueID.to_i, :component => drupalIssueComponent.to_s, :version => drupalIssueVersion.to_s, :status => drupalIssueStatus.to_s, :priority => drupalIssuePriority.to_s, :category => drupalIssueCategory.to_s, :title => entry.title.to_s, :summary => entry.summary.to_s}
 		end
-		puts drupalIssues
 		return drupalIssues
 	end
 
@@ -94,7 +96,7 @@ class DrupalGitHubIssues
 			githubIssueState = x.attrs[:state].to_s
 			issueIDs << {:ghID => githubIssueNumber, :drupalID => drupalIssueNumber, :ghState => githubIssueState }
 		end
-		puts issueIDs
+		# puts issueIDs
 		return issueIDs
 	end
 
@@ -109,32 +111,61 @@ class DrupalGitHubIssues
 		self.add_GH_Comment(repo, issueNumber, "Automated Message...<br>Labels have been updated to latest metadata from Drupal.org")
 	end
 
+
+	def get_Issue_Labels(repo, issueNumber)
+		labelsSawyer = @ghClient.labels_for_issue(repo.to_s, issueNumber.to_i)
+		labels = []
+		labelsSawyer.each do |l|
+			labels << l.attrs[:name]
+		end
+		return labels
+	end
+
 	def determine_Drupal_GH_Changes(repo, drupalData, gitHubData)
 
-		closeStates = ["Fixed", "Closed (fixed)", "Closed (works as designed"]
+		closeStates = ["Fixed", "Closed (fixed)", "Closed (works as designed)"]
 		
 		drupalData.each do |dData|
+			# ghLabels hows the new labels that are being pulled from Drupal.org
 			ghLabels = []
 			ghLabels.push(dData[:component],dData[:version],dData[:status],dData[:priority],dData[:category])
-			puts "Drupal issue Number from dData: #{dData[:issueNumber]}"
-			newArray = gitHubData.find { |ghData| ghData[:drupalID] == dData[:issueNumber]}
-			if newArray == nil
-				self.create_GH_Issue(repo, dData[:title], dData[:summary], :labels => ghLabels)
+			puts ghLabels.sort.to_s
+			
+			# puts "Drupal issue Number from dData: #{dData[:issueNumber]}"
+			newHash = gitHubData.find { |ghData| ghData[:drupalID] == dData[:issueNumber]}
+			# puts "newHash Output:  #{newHash.to_s}"
+			
+			if newHash == nil
+				# "null" is used as a plug -- refactor is needed for optional argument
+				self.create_GH_Issue(dData[:status], repo, dData[:title], dData[:summary], :labels => ghLabels)
 			else
-				if newArray[:ghState] == "open"
+				ghLabelsCurrentApplied = self.get_Issue_Labels(repo, newHash[:ghID])
+				puts ghLabelsCurrentApplied.sort.to_s
+
+				if newHash[:ghState] == "open"
+					if ghLabelsCurrentApplied.sort != ghLabels.sort
+						commentText = "Automated Message... <br> Issue metadata on Drupal.org has been changed. GitHub Labels have been updated to be equivalent to Drupal.org Issue Metadata. <br> Old Labels: #{ghLabelsCurrentApplied} <br> New Labels: #{ghLabels.sort}" 
+						self.update_GH_Issue(repo, newHash[:ghID], commentText, ghLabels)	
+						puts "Change made to GitHub Issue Number: #{newHash[:ghID]}"
+					end
 					if closeStates.include?(dData[:status].to_s) == true
 						commentText = "Automated Message... <br> Issue status on Drupal.org has been changed to #{dData[:status]}.  GitHub Issue State has been updated to be equivalent" 
-						self.update_GH_Issue(repo, newArray[:ghID], commentText, ghLabels)
-						self.close_GH_Issue(repo, newArray[:ghID], dData[:status].to_s)
-						puts "Change made to GitHub Issue Number: #{newArray[:ghID]}"
+						self.update_GH_Issue(repo, newHash[:ghID], commentText, ghLabels)
+						self.close_GH_Issue(repo, newHash[:ghID], dData[:status].to_s)
+						puts "Change made to GitHub Issue Number: #{newHash[:ghID]}"
 					end
-				elsif newArray[:ghState] == "closed"
+				elsif newHash[:ghState] == "closed"
+					if ghLabelsCurrentApplied.sort != ghLabels.sort
+						commentText = "Automated Message... <br> Issue metadata on Drupal.org has been changed. GitHub Labels have been updated to be equivalent to Drupal.org Issue Metadata. <br> Old Labels: #{ghLabelsCurrentApplied} <br> New Labels: #{ghLabels.sort}" 
+						self.update_GH_Issue(repo, newHash[:ghID], commentText, ghLabels)	
+						puts "Change made to GitHub Issue Number: #{newHash[:ghID]}"
+					end
 					if closeStates.include?(dData[:status].to_s) == false
 						reOpenText = "Automated Message... <br> Issue has been reopened, Drupal.org status is now: #{dData[:status]}."
-						self.reOpen_GH_Issue(repo, newArray[:ghID],reOpenText)
+						self.reOpen_GH_Issue(repo, newHash[:ghID],reOpenText)
 						commentText = "Automated Message... <br> Issue status on Drupal.org has been changed to #{dData[:status]}.  GitHub Issue State has been updated to be equivalent" 
-						self.update_GH_Issue(repo, newArray[:ghID], commentText, ghLabels)	
-						puts "Change made to GitHub Issue Number: #{newArray[:ghID]}"
+						self.update_GH_Issue(repo, newHash[:ghID], commentText, ghLabels)	
+						puts "Change made to GitHub Issue Number: #{newHash[:ghID]}"
 					end
 				end
 			end
--- main.rb
+++ main.rb
@@ -1,9 +1,3 @@
 require './Drupal-GitHub.rb'
+start = DrupalGitHubIssues.new("USERNAME","PASSWORD", "GITHUB USER/REPO", "DRUPAL PROJ URL NAME")
 
-start = DrupalGitHubIssues.new
-start.gh_Authenticate("USERNAME","PASSWORD") # ("GitHub Username","GitHub Password")
-# start.get_Drupal_Issues("wetkit", "StephenOTT/Test4") # ("durpal project name (name in url)", "Github OrgUser/RepoName")
-dIssues = start.get_Drupal_Issues("wetkit", "StephenOTT/Test4", false) # ("durpal project name (name in url)", "Github OrgUser/RepoName")
-ghIssues = start.get_GH_Issues("StephenOTT/Test4")
-
-start.determine_Drupal_GH_Changes("StephenOTT/Test4",dIssues,ghIssues)	
